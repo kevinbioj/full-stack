b65cc4f17f400c25effb9914bd866432b811f379
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-01-12 10:21:59 +0100

    fix cors back

--- shop-server/src/main/java/fr/fullstack/shopapp/config/WebSecurityConfig.java
+++ shop-server/src/main/java/fr/fullstack/shopapp/config/WebSecurityConfig.java
@@ -1,21 +1,15 @@
 package fr.fullstack.shopapp.config;
 
-import java.util.List;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebSecurityConfig {
+public class WebSecurityConfig implements WebMvcConfigurer {
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        var configuration = new CorsConfiguration();
-        configuration.setAllowedHeaders(List.of("POST", "GET", "PUT", "DELETE"));
-        var source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+        .allowedMethods("GET", "POST", "PUT", "DELETE");
+  }
 }
